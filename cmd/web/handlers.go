b8fd9b2d6e7e2903ad0e3ec2abc394898fad89af
Nirjan Khadka <webdev.nk@gmail.com>
2025-12-18 11:59:10 +0545

    Render latest snippets on home page
    This commit updates the `home` handler to fetch the latest snippets and
    render them using templates.
    
    It also introduces a new `templateData` struct that can hold a slice of
    snippets for the home page template. The `showSnippet` template has been
    updated to use a `with` block for cleaner access to snippet data.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,32 +11,30 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	// files := []string{
-	// 	"./ui/html/home.page.tmpl",
-	// 	"./ui/html/base.layout.tmpl",
-	// 	"./ui/html/footer.partial.tmpl",
-	// }
-
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// 	http.Error(w, "Internal Server Error", 500)
-	// 	return
-	// }
-	// err = ts.Execute(w, nil)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// }
-
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
+	data := &templateData{Snippets: s}
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
 	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = ts.Execute(w, data)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
