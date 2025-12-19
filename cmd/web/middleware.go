7da98e44374a32476269b8028cf11cb01f24449e
Nirjan Khadka <webdev.nk@gmail.com>
2025-12-19 10:40:16 +0545

    Refactor authentication to use context

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/justinas/nosurf"
+	"nirjan.dev/snippetbox/pkg/models"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -46,6 +49,29 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 
 		w.Header().Add("Cache-Control", "no-store")
 		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		exists := app.session.Exists(r, "authenticatedUserID")
+		if !exists {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		user, err := app.users.Get(app.session.GetInt(r, "authenticatedUserID"))
+		if errors.Is(err, models.ErrNoRecord) || !user.Active {
+			app.session.Remove(r, "authenticatedUserID")
+			next.ServeHTTP(w, r)
+			return
+		} else if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), contextKeyIsAuthenticated, true)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
 
